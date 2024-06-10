23741882a632fc44ae783b155dd0394f70af1fc2
Syarif09 <syarif.khalid@gmail.com>
2024-06-10 21:10:11 +0700

    revisi

--- task4.js
+++ task4.js
@@ -1,31 +1,47 @@
 // Mengurutkan bilangan dengan angka 0 sebagai pemisah
 
-// const divideAndSort = function () {
+const divideAndSort = function (angka) {
     
-// }
-
-let angka = 748303187
-let strAngka = angka+''
-let ba, bb = 0
-
-let hasil = ''
-
-for (let i = 0; i <= strAngka.length; i++){
-    if (strAngka[i] === '0' || i == strAngka.length) {
-        ba = i
-        let temp = []
-        for (bb; bb < ba; bb++) {
-            temp = [...temp, +strAngka[bb]]
+    let strAngka = angka+''
+    let ba, bb = 0
+    
+    let hasil1 = ''
+    
+    for (let i = 0; i <= strAngka.length; i++){
+        // let data = []
+        if (strAngka[i] === '0' || i == strAngka.length) {
+            ba = i
+            let temp = []
+            for (bb; bb < ba; bb++) {
+                temp = [...temp, +strAngka[bb]]
+            }
+            // Urutkan hasil filter
+            for (let i = 0; i < temp.length - 1; i++) {
+                for (let j = 0; j < temp.length - 1 - i; j++) {
+                    if (temp[j] > temp[j + 1]) {
+                        let result = temp[j];
+                        temp[j] = temp[j + 1];
+                        temp[j + 1] = result;
+                    }
+                }
+            }
+            ba = bb + 1
+            bb = bb + 1
+            hasil1 = [...hasil1, temp]
         }
-
-        console.log(temp)
-        ba = bb + 1
-        bb = bb + 1
-        hasil += temp
-
     }
+    
+    let hasil2 = ''
+    for (let i = 0; i < hasil1.length; i++) {
+        for (let j = 0; j < hasil1[i].length; j++) {
+            hasil2 += hasil1[i][j]
+        }
+    }
+    
+    let hasil3 = +hasil2
+    
+    console.log(hasil3)
+    console.log(typeof hasil3)
 }
-console.log(hasil)
 
-// let convered = +strAngka
-// console.log(typeof convered)
+divideAndSort(32410650978)
